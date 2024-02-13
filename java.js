1dfbc49ecb3992e85d538a54964f53d71e89a0b4
Mahir Nanjib Chowdhury <mahirnanjib166@gmail.com>
2024-02-13 15:25:46 +0600

    cleared everything because it became too messy

---
+++ java.js
@@ -0,0 +1,12 @@
+
+// putting every class in various variables so that we can use them when needed
+
+let attemptP =document.querySelector(".attempt-p")
+let loosingP =document.querySelector(".loosing-p")
+let winningP =document.querySelector(".winning-p")
+let submitButton =document.querySelector(".submit-button")
+let inputBox =document.querySelector(".input-box")
+let instructionP =document.querySelector(".instruction-p")
+let mainForm =document.querySelector(".main-form")
+let mainHeading =document.querySelector(".main-heading")
+let elementHolder =document.querySelector(".element-holder")
